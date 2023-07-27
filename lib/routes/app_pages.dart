ff4830348abfa63a03e472183246954ee4b70598
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-07-27 15:34:23 +0100

    add_page_routes

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
 import 'package:tawba/routes/app_routes.dart';
+import 'package:tawba/screens/ahadith/ahadith_page.dart';
+import 'package:tawba/screens/azkar/azkar_page.dart';
+import 'package:tawba/screens/god_names/god_names_page.dart';
+import 'package:tawba/screens/quran/quran_page.dart';
 import 'package:tawba/screens/splash/splash_page.dart';
 import '../screens/home/home_page.dart';
 
@@ -12,6 +16,22 @@ class AppPages {
     GetPage(
       name: AppRoutes.HOME,
       page: () => const HomePage(),
+    ),
+    GetPage(
+      name: AppRoutes.AZKAR,
+      page: () => const AzkarPage(),
+    ),
+    GetPage(
+      name: AppRoutes.AHADITH,
+      page: () => const AhadithPage(),
+    ),
+    GetPage(
+      name: AppRoutes.QURAN,
+      page: () => const QuranPage(),
+    ),
+    GetPage(
+      name: AppRoutes.GODNAMES,
+      page: () => const GodNamesPage(),
     ),
   ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
 abstract class AppRoutes {
   static const SPLASH = '/';
   static const HOME = '/home';
+  //azkar
+  static const AZKAR = '/azkar';
+  //ahadith
+  static const AHADITH = '/ahadith';
+  //godNames
+  static const GODNAMES = '/god_names';
+
+  //quran
+  static const QURAN = '/quran';
 }
