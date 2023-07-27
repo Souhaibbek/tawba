5fe6917c690ce9d2b4c287f3b85ebcc94cd4b2d5
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-07-27 15:33:48 +0100

    add_homepage

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tawba/routes/app_routes.dart';
+import 'package:tawba/screens/home/home_controller.dart';
+import 'package:tawba/widgets/global_appbar.dart';
 
-class HomePage extends StatelessWidget {
+import '../../widgets/menu_item_box.dart';
+
+class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      appBar: const GlobalAppBar(
+        title: 'الرئيسية',
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MenuItemBox(
+                title: 'القرآن الكريم',
+                onTap: () => Get.toNamed(AppRoutes.QURAN),
+              ),
+              MenuItemBox(
+                title: 'اذكار المسلم',
+                onTap: () => Get.toNamed(AppRoutes.AZKAR),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MenuItemBox(
+                title: 'اسماء الله الحسنه',
+                onTap: () => Get.toNamed(AppRoutes.GODNAMES),
+              ),
+              MenuItemBox(
+                title: 'احاديث نبوية',
+                onTap: () => Get.toNamed(AppRoutes.AHADITH),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
